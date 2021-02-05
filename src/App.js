24cb83841b4e276986be1245bc8d8391575f021c
Hunter Foist <hfoist@gmail.com>
2021-02-04 20:04:43 -0500

    RickMorty Fetch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import MortyParent from './components/Morty/MortyParent'
 
 function App() {
   return (
     <div>
+      <MortyParent />
     </div>
   );
 }
---
+++ src/components/Morty/MortyChild.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import {
+    Card, Button, CardImg, CardTitle, CardText, CardDeck,
+    CardSubtitle, CardBody
+  } from 'reactstrap';
+
+const MortyChild = (props) => {
+    return ( 
+        <Card>
+        <CardImg top width="100%" src={props.mortyCharacter.image} alt="Card image cap" />
+        <CardBody>
+          <CardTitle tag="h5">{props.mortyCharacter.name}</CardTitle>
+          <CardSubtitle tag="h6" className="mb-2 text-muted">{props.mortyCharacter.species}</CardSubtitle>
+          {/* <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText> */}
+          <Button>More Info</Button>
+        </CardBody>
+      </Card>
+ );
+}
+ 
+export default MortyChild;
